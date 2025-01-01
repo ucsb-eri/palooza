efd7719c22cd3d6ce283f4cbb95a9f0e88ebdea0
Tjaart van der Walt <git@tjaart.org>
2024-12-31 20:50:43 -0800

    Frontend: add useFetch

--- palooza/frontend/src/components/PaloozaBoard.vue
+++ palooza/frontend/src/components/PaloozaBoard.vue
@@ -1,6 +1,36 @@
 <script setup>
 import { ref } from 'vue'
+import { useFetch } from '@vueuse/core'
 import PaloozaColumn from './PaloozaColumn.vue'
+
+const url = ref('http://localhost:1234/')
+const refetch = ref(false)
+
+// const toggleRefetch = useToggle(refetch)
+
+const { data, error, abort, statusCode, isFetching, isFinished, canAbort, execute } = useFetch(
+  url,
+  { refetch },
+).get()
+
+console.log(data)
+
+// const text = stringify(reactive({
+// isFinished,
+// isFetching,
+// canAbort,
+// statusCode,
+// //   error,
+// //   data: computed(() => {
+// //     try {
+// //       return JSON.parse(data.value as string)
+// //     }
+// //     catch (e) {
+// //       return null
+// //     }
+// //   }),
+// }))
+
 const cards = ref([
   { id: 1, title: 'Task 1', status: 'New' },
   // Add more cards with various statuses
