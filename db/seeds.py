50df87f20c454cfcf357ab5b4a8e8699e69b3068
Tjaart van der Walt <git@tjaart.org>
2025-01-06 10:43:10 -0800

    Add OS details

--- db/seeds.py
+++ db/seeds.py
@@ -13,16 +13,31 @@ if palooza is None:
     palooza = Palooza(**palooza_dict).save()
 
 
-node_1 = {"name": "server1", "palooza": palooza}
+node_1 = {"name": "server1", 
+
+    "os_name": "fedora",
+    "os_version": "41 (Forty One)",
+          "palooza": palooza}
 if Node.find_by_name(node_1["name"]) is None:
     Node(**node_1).save()
 
 node_2 = {
     "name": "server2",
+    "os_name": "ubuntu",
+    "os_version": "24.04",
     "palooza": palooza,
 }
 if Node.find_by_name(node_2["name"]) is None:
     Node(**node_2).save()
+
+node_3 = {
+    "name": "server3",
+    "os_name": "centos",
+    "os_version": "7 (Core)",
+    "palooza": palooza,
+}
+if Node.find_by_name(node_3["name"]) is None:
+    Node(**node_3).save()
 
 current_palooza = {"name": "current_palooza", "value": str(palooza.id)}
 
